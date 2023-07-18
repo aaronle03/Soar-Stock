126165db194ee9bb8c70efc44e1dcd9652ba5fdd
Aaron Le <91503688+aaronle03@users.noreply.github.com>
2023-07-17 21:35:56 -0700

    first commit, navbar sorta done, not clean

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+import Navbar from '../components/Navbar';
+
+function Home() {
+    return (
+        <div className="home">
+            <Navbar />
+            <div className="home-container">
+                HELLO AAAAAAAAAAAAAAAAAAAAAAAAAA
+            </div>
+        </div>
+    );
+}
+
+export default Home;
