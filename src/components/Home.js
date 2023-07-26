1090853a0223a760eb37ab2f47c81bbe754a1a71
Aaron Le <91503688+aaronle03@users.noreply.github.com>
2023-07-26 14:43:32 -0700

    initial files, navbar redone

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,15 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './Home.css';
 import Navbar from '../components/Navbar';
 
-// Functions
-import { getTest } from '../functions/test';
-
 function Home() {
-    const [data, setData] = useState("Hello World!");
-
-    useEffect(() => {
-        getTest()
-            .then((res) => {
-                setData(res.message);
-                console.log(res);
-            })
-            .catch((err) => console.log(err));
-    });
-
     return (
-        <div className="home">
-            <Navbar />
-            <div className="home-container">
-                <h1>{data}</h1>
-                <button>TEST</button>
+        <div class="home">
+            <div class="home-navbar">
+                <Navbar></Navbar>
+            </div>
+            <div class="home-content-container">
+                <h1>Hello</h1>
             </div>
         </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,19 @@ function Navbar() {
   return (
       <nav className="nav">
           <section className='logo'>
-            <h1>STOCK SOAR</h1>
-            <img src="https://icongr.am/jam/plane.svg?size=40&color=E0E1DD" alt="Avatar" border="0" />
+            <section className='logo-title'>
+              <h1>SOAR STOCK</h1>
+              <h3>CRYPTOEXCHANGE</h3>
+            </section>
+            <img src="https://icongr.am/entypo/rocket.svg?size=70&color=921e25" alt="Avatar" border="0" />
           </section>
           <section className='navbar-links'>
                 <a href="https://www.google.com/">HOME</a>
-                <a href="https://www.google.com/">TRADE</a>
-                <a href="https://www.google.com/">PORTFOLIO</a>
-                <a href="https://www.google.com/">CONTACT</a>
+                <a href="https://www.google.com/">MARKETS</a>
+                <a href="https://www.google.com/">ASSETS</a>
+                <a href="https://www.google.com/">ACCOUNT</a>
+                <a href="https://www.google.com/">DISCOVER</a>
+                <a href="https://www.google.com/">SUPPORT</a>
           </section>
           <section className='profilejoin'>
             <a href="https://github.com/aaronle03" className="profile-link">
